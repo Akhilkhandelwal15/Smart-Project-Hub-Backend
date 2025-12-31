267ff02cd3c4b315a475550d023df60656936917
Akhilkhandelwal15 <khandelwalakhil2003@gmail.com>
2025-12-31 10:23:20 +0530

    initial commit: project setup and authcontroller setup

---
+++ controllers/authController.js
@@ -0,0 +1,49 @@
+import User from "../models/User.js";
+import { generateToken } from "../utils/jwt.js";
+
+export const register = async(req, res)=>{
+  const {name, email, password, role} = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(404).json({ success: false, message: "All fields are required" });
+  }
+
+  try{
+    const userExists = await User.findOne({email});
+    if(userExists) return res.status(400).json({success: false, message:'User already exists'});
+
+    const user = await User.create({name, email, password, role});
+    const token = generateToken(user);
+    res.status(200).json({
+      success: true, 
+      message:'User successfully created', 
+      user:{ id: user._id, name: user.name, email: user.email, role: user.role }, 
+      token
+    });
+  }
+  catch(error){
+    res.status(500).json({success: false, message:'Unexpected Server error'});
+  }
+}
+
+export const login = async(req, res)=>{
+  const {email, password} = req.body;
+  try{
+    const user = await User.findOne({email});
+    if(!user) return res.status(400).json({success: false, message: "Invalid credentials"});
+
+    const isMatch = await User.comparePassword(password);
+    if(!isMatch) return res.status(400).json({success: false, message: 'Invalid credentials'});
+
+    const token = generateToken(user);
+    res.status(200).json({
+      success: true, 
+      message:'User successfully logged in', 
+      user:{ id: user._id, name: user.name, email: user.email, role: user.role }, 
+      token
+    });
+  }
+  catch(error){
+    res.status(500).json({success: false, message:'Unexpected Server error'});
+  }
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+const PORT = 3000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+// connect DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(()=> console.log("MongoDB connected"))
+  .catch((error)=>console.log(error));
+
+// routes
+app.use('/api/auth', authRoutes);
+app.get('/', (req, res)=>{
+  res.send('Server is running.');
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port ${PORT}`);
+});
